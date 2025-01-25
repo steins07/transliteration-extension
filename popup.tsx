e12d34020eff7ed8a9850cd76e524dbd244d0d7e
Romus Shrestha <sromus498@gmail.com>
2025-01-25 16:48:24 +0545

    minor changes

--- popup.tsx
+++ popup.tsx
@@ -1,21 +1,26 @@
 import { useState } from "react"
+import { getNepaliFromEnglish } from 'nepali-input-react';
 
 function IndexPopup() {
   const [data, setData] = useState("")
-
+  function handleChangeOnSpaceKey(e: any) {
+    if (e.keyCode === 32||e.key===' ') {
+      e.target.value = getNepaliFromEnglish(e.target.value)
+    }
+  }
   return (
     <div
       style={{
         padding: 16
       }}>
       <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
+        Roman to Devnagari
         Extension!
       </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
+      
+      <input onChange={(e) => setData(e.target.value)} 
+      onKeyDown={handleChangeOnSpaceKey} value={data} />
+      <p>{getNepaliFromEnglish(data)}</p>
       <a href="https://docs.plasmo.com" target="_blank">
         View Docs
       </a>
